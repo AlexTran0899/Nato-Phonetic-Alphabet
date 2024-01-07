49ac18a69c9a9b9b9e6c5d275899813ff1018bae
Alex Tran <alextran0899@gmail.com>
2024-01-07 12:22:07 -0600

    Initial Commit

---
+++ Nato phonetic alphabet/Nato_phonetic_alphabetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nato_phonetic_alphabetApp.swift
+//  Nato phonetic alphabet
+//
+//  Created by Alex Tran on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Nato_phonetic_alphabetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
